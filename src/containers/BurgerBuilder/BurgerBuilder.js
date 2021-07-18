dc97c8afa94f81ebd9ff40a21475f84e13ab676c
Amaan Iqbal <amaaniqbal@MacBook-Pro.local>
2021-07-18 18:27:34 +0530

    added structure and ejected the project...added some code too

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React , {Component} from 'react';
+import Aux from '../../hoc/aux';
+
+
+class BurgerBuilder extends Component{
+    render(){
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+
+        );
+    }
+}
+
+export default BurgerBuilder;
