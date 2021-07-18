1db3a22e5d7f0cb447d951b40dfcc83e631af5c9
Amaan Iqbal <amaaniqbal@MacBook-Pro.local>
2021-07-18 21:42:45 +0530

    Added Structure and added BurgerIngredient code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from 'react';
+import Layout from '../src/components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+          <Layout>
+            <BurgerBuilder/>
+          </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
